8cb1ac290c6148830705d49562dc01f70ac10d11
Awesomeness7567 <joh22044@byui.edu>
2023-09-16 21:39:12 -0600

    home page

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+let today1 = new Date();
+document.querySelector("#currentyear").textContent=today1.getFullYear()
+document.querySelector("#lastmodified").textContent=document.lastModified
+
+//document.getElementById("currentyear").innerHTML=new Date().getFullYear();
+//document.getElementById("lastmodified").innerHTML=new Date(document.lastModified).toLocaleDateString();
