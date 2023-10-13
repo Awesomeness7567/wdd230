6ec2b7c656827c12875c25374876ad2214b842f7
Awesomeness7567 <joh22044@byui.edu>
2023-10-12 18:26:18 -0600

    js

--- chamber/scripts/dark_mode.js
+++ chamber/scripts/dark_mode.js
@@ -3,9 +3,3 @@ const toggleSwitch = document.querySelector('#dark-mode-toggle');
 toggleSwitch.addEventListener('change', () => {
     document.body.classList.toggle('dark-mode');
 });
-
-// darkmode.addEventListener('click', () => {
-//     if (darkmode.textContent == 'DARK') {
-//         document.documentElement.style.setProperty('--text-color','black');
-//     }
-// })
