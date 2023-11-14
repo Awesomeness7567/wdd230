55400bba9a653553e6f6ce050636b07e10169399
Awesomeness7567 <joh22044@byui.edu>
2023-11-13 18:10:33 -0700

    directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,44 +1,46 @@
-const url = "./data/members.json";
+// Grid vs list
 
-// const displayBusinessesCard = (businesss) => {
-//     const cards = document.querySelector(".directory-cards"); // select the output container element
+var gridSelector = document.querySelector('#directory-grid');
+var listSelector = document.querySelector('#directory-list');
+var directoryData = document.querySelector('#directory-data');
 
-//     businesss.forEach((business) => {
-//         // Create elements to add to the div.cards element
-//         let card = document.createElement("section");
-//         card.innerHTML = `
-//         <img src="${business.imageURL}">
-//         <p>${business.name}</p>
-//         <p>${business.streetAddress}</p>
-//         <p>${business.cityStateZip}</p>
-//         <p><a class="card-button" href="${business.websiteURL}">Website</a></p>
-//         `;
-//         if (business.membershipLevel=='gold'){
-//           card.classList.add('gold-member');
-//         }
-//         cards.appendChild(card);
-//       }); // end of forEach loop
-      
-//     }; // end of function expression
+gridSelector.addEventListener('click', ()=>{
+    if (!gridSelector.classList.contains('active')){    
+        gridSelector.classList.add('active');
+        listSelector.classList.remove('active');
+        directoryData.classList.add('directory-cards')
+        directoryData.classList.remove('directory-list')
+    }
+});
 
+listSelector.addEventListener('click', ()=>{
+    if (!listSelector.classList.contains('active')){
+        listSelector.classList.add('active');
+        gridSelector.classList.remove('active');
+        directoryData.classList.add('directory-list')
+        directoryData.classList.remove('directory-cards')
+    }
+});
+
+
+// members data
+
+const url = "members.json";
 
 const displayHashirasCard = (members) => {
     const cards = document.querySelector(".directory-cards"); // select the output container element
         
             members.forEach(member => {
                 let section = document.createElement("section")
-                // section.classList.add("card")
-                section.innerHTML = `<h3>${member.name}</h3>
+                section.innerHTML = `<h5>${member.name}</h5>
                     <p>Address: ${member.address}</p>
                     <p>Kasugai Crow: ${member.communication}</p>
-                    <img src="${member.imageURL}">
+                    <img class="hashira-img" src="${member.imageURL}">
                     <p>${member.membershipLevel}</p>
-                    <p><a class="card-button" href="${member.websiteURL}">Website</a></p>`
-                    section.innerHTML = sectionHTML;
+                    <button><a class="card-button" href="${member.websiteURL}">Website</a></button>`
                     cards.appendChild(section);
             });
 }
-
 
 async function getHashiraData() {
     const response = await fetch(url);
@@ -47,7 +49,7 @@ async function getHashiraData() {
         displayHashirasCard(data.members);
     }
     else{
-        console.error("There was an error loading the data!!")
+        console.error("There was an error loading the data!!");
     }
 }
 
