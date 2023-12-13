fc29ca13c07bed8c244a159c91d7c3dc11701741
Awesomeness7567 <joh22044@byui.edu>
2023-12-12 22:59:57 -0700

    storage

--- bountiful_foods/scripts/drink_number.js
+++ bountiful_foods/scripts/drink_number.js
@@ -1,44 +1,11 @@
-// function storeClickCount() {
-//     if (localStorage.clickCount) {
-//       localStorage.clickCount = Number(localStorage.clickCount) + 1;
-//     } else {
-//       localStorage.clickCount = 1;
-//     }
-//   }
+let submitCount = parseInt(localStorage.getItem('submitCount')) || 0;
+let submitCountElement = document.getElementById('submitCount');
 
-// var clickCount = localStorage.clickCount;
+function submitForm() {
+  submitCount++;
+  localStorage.setItem('submitCount', submitCount);
+  console.log("Form submitted " + submitCount + " times.");
+  submitCountElement.textContent = submitCount;
+}
 
-
-// function storeClickCount() {
-//     if (localStorage.clickCount) {
-//       localStorage.clickCount = Number(localStorage.clickCount) + 1;
-//     } else {
-//       localStorage.clickCount = 1;
-//     }
-//     alert("The form has been submitted " + localStorage.clickCount + " times.");
-// }
-
-
-// function myfunc(specialty_drinks) {
-//     specialty_drinks.preventDefault();
-
-//     var first_name = document.getElementById('firstname').value;
-//     localStorage.setItem('ls_first_name', first_name);
-//     var firstName = localStorage.getItem('ls_first_name');
-//     console.log(firstName);
-// }
-
-  function submitForm() {
-    var submitCount = 0;
-    if (typeof(Storage) !== "undefined") {
-      if (localStorage.submitCount) {
-        submitCount = Number(localStorage.submitCount) + 1;
-      } else {
-        submitCount = 1;
-      }
-      localStorage.setItem("submitCount", submitCount);
-      document.write("The form was submitted " + submitCount + " times.");
-    } else {
-      document.write("Sorry, your browser does not support web storage...");
-    }
-  }
+submitCountElement.textContent = submitCount;
