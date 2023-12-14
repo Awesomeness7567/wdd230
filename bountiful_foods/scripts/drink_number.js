62dd7dd5ddc2e5705546b22581bfff809892a904
Awesomeness7567 <joh22044@byui.edu>
2023-12-13 21:47:18 -0700

    count

--- bountiful_foods/scripts/drink_number.js
+++ bountiful_foods/scripts/drink_number.js
@@ -1,11 +1,20 @@
-let submitCount = parseInt(localStorage.getItem('submitCount')) || 0;
-let submitCountElement = document.getElementById('submitCount');
+// Get the form element
+const form = document.querySelector('form');
 
-function submitForm() {
-  submitCount++;
-  localStorage.setItem('submitCount', submitCount);
-  console.log("Form submitted " + submitCount + " times.");
-  submitCountElement.textContent = submitCount;
+// Get the count from local storage
+let count = localStorage.getItem('formSubmitCount');
+
+// If the count is not set, set it to 0
+if (count === null) {
+  count = 0;
 }
 
-submitCountElement.textContent = submitCount;
+// Increment the count when the form is submitted
+form.addEventListener('submit', () => {
+  count++;
+  localStorage.setItem('formSubmitCount', count);
+  document.querySelector('#submit-count').textContent = count;
+});
+
+// Display the count in HTML
+document.querySelector('#submit-count').textContent = localStorage.getItem('formSubmitCount');
